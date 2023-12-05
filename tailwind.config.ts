16f4bd9460028641adb3d8474c7444652818698d
Danielle Heckman <danielleheckman5@gmail.com>
2023-12-05 11:43:54 -0500

    updating tailwild

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,28 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
+import colors from 'tailwindcss/colors';
 
 export default {
   content: ['./app/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main-green': '#D0E0B5',
+        'button-red': '#CD0100'
+      },
+      height: {
+        16: '16.125rem'
+      },
+      maxWidth: {
+        16: '16.25rem'
+      },
+      width: {
+        16: '16.25rem',
+        27: '27rem'
+      }
+    },
+    fontFamily: {
+      inter: ['Inter']
+    }
   },
-  plugins: [],
-} satisfies Config
-
+  plugins: []
+} satisfies Config;
