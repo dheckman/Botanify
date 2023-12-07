e56521ef9371baf86b65ffc2b1b0f3f309588fe6
Danielle Heckman <danielleheckman5@gmail.com>
2023-12-07 15:12:52 -0500

    tailwind updates

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,17 +7,8 @@ export default {
     extend: {
       colors: {
         'main-green': '#D0E0B5',
-        'button-red': '#CD0100'
-      },
-      height: {
-        16: '16.125rem'
-      },
-      maxWidth: {
-        16: '16.25rem'
-      },
-      width: {
-        16: '16.25rem',
-        27: '27rem'
+        'button-red': '#CD0100',
+        'overlay-gray': '#D9D9D9'
       }
     },
     fontFamily: {
